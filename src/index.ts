d18f2e455dec55db403fa82414beee83be236284
LacourVincent <lacour.vincent.app@gmail.com>
2019-07-06 20:12:05 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import count from './count';
 import sortAsc from './sortAsc';
 import sortDesc from './sortDesc';
+import isAlpha from './isAlpha';
+import isDigit from './isDigit';
 
-export { count, sortAsc, sortDesc };
+export { count, sortAsc, sortDesc, isAlpha, isDigit };
