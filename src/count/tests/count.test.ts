dd1b8cd82ccfa1dfbd5602dd3677ca2fdabbf5d8
Gautier Darchen <darchen.gautier@gmail.com>
2019-07-25 15:31:48 +0200

    feat(count): enhance the count function to count the number of items matching a predicate

--- src/count/tests/count.test.ts
+++ src/count/tests/count.test.ts
@@ -1,5 +1,5 @@
 import count from '../';
-import { BOOLEAN_ARRAY, NUMBER_ARRAY, STRING_ARRAY } from './mocks';
+import { BOOLEAN_ARRAY, NUMBER_ARRAY, STRING_ARRAY, OBJECT_ARRAY, RANDOM_TYPED_ARRAY } from './mocks';
 
 // Array<number> tests
 test('count(NUMBER_ARRAY, 1) to equal 2', () => {
@@ -12,6 +12,10 @@ test('count(NUMBER_ARRAY, 42) to equal 0', () => {
 
 test('count(NUMBER_ARRAY, 5) to equal 3', () => {
   expect(count(NUMBER_ARRAY, 5)).toBe(3);
+});
+
+test('count(NUMBER_ARRAY, (item) => item.name === "js") to equal 0', () => {
+  expect(count(NUMBER_ARRAY, item => item.name === 'js')).toBe(0);
 });
 
 // Array<string> tests
@@ -27,6 +31,10 @@ test('count(STRING_ARRAY, "world") to equal 3', () => {
   expect(count(STRING_ARRAY, 'world')).toBe(3);
 });
 
+test('count(STRING_ARRAY, (item) => item.name === "js") to equal 0', () => {
+  expect(count(STRING_ARRAY, item => item.name === 'js')).toBe(0);
+});
+
 // Array<boolean> tests
 test('count(BOOLEAN_ARRAY, true) to equal 3', () => {
   expect(count(BOOLEAN_ARRAY, true)).toBe(3);
@@ -38,6 +46,36 @@ test('count(BOOLEAN_ARRAY, 42) to equal 0', () => {
 
 test('count(BOOLEAN_ARRAY, false) to equal 1', () => {
   expect(count(BOOLEAN_ARRAY, false)).toBe(1);
+});
+
+test('count(BOOLEAN_ARRAY, (item) => item.name === "js") to equal 0', () => {
+  expect(count(BOOLEAN_ARRAY, item => item.name === 'js')).toBe(0);
+});
+
+// Array<object> tests
+test('count(OBJECT_ARRAY, (item) => item.name === "js") to equal 2', () => {
+  expect(count(OBJECT_ARRAY, item => item.name === 'js')).toBe(2);
+});
+
+test('count(OBJECT_ARRAY, 42) to equal 0', () => {
+  expect(count(OBJECT_ARRAY, 42)).toBe(0);
+});
+
+test('count(OBJECT_ARRAY, () => null to equal 0', () => {
+  expect(count(OBJECT_ARRAY, () => null)).toBe(0);
+});
+
+test('count(OBJECT_ARRAY, () => true to equal OBJECT_ARRAY.length', () => {
+  expect(count(OBJECT_ARRAY, () => true)).toBe(OBJECT_ARRAY.length);
+});
+
+// Array<any>
+test('count(RANDOM_TYPED_ARRAY, (item) => item.name === "js" to equal 2', () => {
+  expect(count(RANDOM_TYPED_ARRAY, item => item.name === 'js')).toBe(2);
+});
+
+test('count(RANDOM_TYPED_ARRAY, "TS" to equal 2', () => {
+  expect(count(RANDOM_TYPED_ARRAY, 'TS')).toBe(1);
 });
 
 //Exceptions tests
--- src/count/tests/mocks.ts
+++ src/count/tests/mocks.ts
@@ -3,3 +3,7 @@ export const NUMBER_ARRAY = [1, 5, 7, 4, 6, 1, 5, 5];
 export const STRING_ARRAY = ['hello', 'world', 'world', 'JS rocks', 'TS rocks better', 'hello', 'world'];
 
 export const BOOLEAN_ARRAY = [true, true, false, true];
+
+export const OBJECT_ARRAY = [{ id: 1, name: 'js' }, { id: 2, name: 'extra' }, { id: 3, name: 'js' }];
+
+export const RANDOM_TYPED_ARRAY = [{ id: 1, name: 'js' }, 'TS', { id: 2, name: 'js' }];
